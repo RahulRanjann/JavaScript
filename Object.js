20ffb649b512a23d4aa46342ba251f9223f41cfa
Rahul Ranjan <88098666+RahulRanjann@users.noreply.github.com>
2023-07-04 00:13:52 +0530

    objecet
    object

---
+++ Object.js
@@ -0,0 +1,17 @@
+const item = {
+    name: "Biscuit",
+    type: "Regular",
+    category: "Food",
+    price: 200,
+    Expires: false,
+    discount: undefined,
+
+}
+
+console.log(item.name);
+console.log(item.type);
+console.log(item.category);
+console.log(item.price);
+console.log(item.Expires);
+console.log(item.discount);
+
